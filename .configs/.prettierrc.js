36fd1b4bb8b09f2f1802e961b7437b64e7c355fd
Yet.it.Compiles <Ty.Unsworth@gmail.com>
2025-01-06 23:59:32 -0800

    🔧 Config: Initial commit of project config files

---
+++ .configs/.prettierrc.js
@@ -0,0 +1,32 @@
+/**
+ * @file .prettierrc.js
+ *
+ * @version 1.0.0
+ *
+ * @summary Prettier Formatting Configuration File
+ *
+ * @description Defines consistent code styling and file formatting using
+ * Prettier across the projects stack.
+ *
+ * @requires prettier
+ *
+ * @see https://prettier.io/docs/en/ link to official prettier documentation
+ *
+ * @exports {object} Prettier configuration object.
+ */
+export default {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'consistent',
+    bracketSpacing: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'always',
+    endOfLine: 'lf',
+    htmlWhitespaceSensitivity: 'css',
+};
